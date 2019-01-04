3f7c0df0a13f6d8da8ff3c9d263ffdd94de95c7a
Angelika Ziolkowska <angelikaenquiries@gmail.com>
2019-01-04 17:50:53 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 
 /// <summary>
 /// Card game where cards are dealt out to a group of players so that each player 
-/// has the same number of cards, then displays each player’s hand and the total 
+/// has the same number of cards, then displays each playerâ€™s hand and the total 
 /// value of their hand in ranked order. 
 /// </summary>
-namespace ConsoleApp1
+namespace ConsoleApp
 {
 
     enum CardName
